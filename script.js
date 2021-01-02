972448f4af57ba78594718e6108f5050112bbf56
gnwncpta <gnwncpta@gmail.com>
2021-01-03 05:01:24 +0700

    New Code

---
+++ script.js
@@ -0,0 +1,16 @@
+// let i = 10
+// console.log(i);
+
+// function ulang(){
+//         for( var i = 0; i < 10; i++ ){
+//         console.log(i);
+//     }
+// }
+// ulang();
+// console.log(i);\
+
+for( let i = 0; i < 10; i++ ){
+    console.log(i);
+}
+
+console.log(i);
